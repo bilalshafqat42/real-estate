1a329c62ad945e3aaaee631e40e2dba357d4f339
bilalshafqat42 <bilalshafqat42@gmail.com>
2024-02-06 12:31:42 +0400

    new routes and pages created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
 
 const App = () => {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline text-red-50">Hello world!</h1>{" "}
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-out" element={<SignOut />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
